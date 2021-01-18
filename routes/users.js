cb643db6d0634f3ac2d870721b49217a5350ee5e
Mathias Valverde <mathiasvalverde@hotmail.fr>
2021-01-18 17:53:03 +0100

    user route update

--- routes/users.js
+++ routes/users.js
@@ -7,7 +7,11 @@ const Lesson = require("../models/Lesson");
 const Event = require("../models/Event");
 
 router.get("/", function (req, res, next) {
-  res.send("respond with a resource");
+  Users.find({})
+    .populate("id_user")
+    .then((userDocuments) => {
+      res.status(200).json(users);
+    });
 });
 
 router.get("/me", (req, res, next) => {
