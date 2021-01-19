983c36a89f6e1e6f94e0ea52fb9c73c3594e4e1c
Mathias Valverde <mathiasvalverde@hotmail.fr>
2021-01-19 11:51:03 +0100

    updated lesson model categories

--- models/Lesson.js
+++ models/Lesson.js
@@ -1,18 +1,19 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const lessonSchema = new Schema({
-  name: String,
-  category: { type: String, enum: ["TBD", "TBD"] },
-  difficulty: String,
-  content: String,
-  likes: Number,
-  trainerId: {
-    type: Schema.Types.ObjectId,
-    ref: "User",
-  }, //lesson.find
-},
-  {timestamps: { createdAt: "created_at", updatedAt: "updated_at" }}
+const lessonSchema = new Schema(
+  {
+    name: String,
+    category: { type: String, enum: ["Hacking", "Network", "Programming"] },
+    difficulty: String,
+    content: String,
+    likes: Number,
+    trainerId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    }, //lesson.find
+  },
+  { timestamps: { createdAt: "created_at", updatedAt: "updated_at" } }
 );
 
 const Lesson = mongoose.model("Lesson", lessonSchema);
