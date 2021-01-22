43a1bf222c3d4a8d993aa560196c99ab68f31b02
Mathias Valverde <mathiasvalverde@hotmail.fr>
2021-01-22 10:31:56 +0100

    heroku update

---
+++ public/precache-manifest.ff8f3988f061d2473231e57cdd340e08.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3bfbbca8f1ac640cdab16f9065347ae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "59ccaf066fe8e95d58af",
+    "url": "/static/css/2.987b09ae.chunk.css"
+  },
+  {
+    "revision": "30e6dedcb6cfe7dae79c",
+    "url": "/static/css/main.852a7801.chunk.css"
+  },
+  {
+    "revision": "59ccaf066fe8e95d58af",
+    "url": "/static/js/2.aaa48dd7.chunk.js"
+  },
+  {
+    "revision": "920b26c443f96f4c515b6b34276de648",
+    "url": "/static/js/2.aaa48dd7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30e6dedcb6cfe7dae79c",
+    "url": "/static/js/main.7899eb65.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "345d1b5d3c61f0e7841b6d1c6009f7f4",
+    "url": "/static/media/background.345d1b5d.jpg"
+  },
+  {
+    "revision": "8406ec9a141bb211a48a380a9e860100",
+    "url": "/static/media/logo_dedsec_white.8406ec9a.png"
+  },
+  {
+    "revision": "771e76871abe0a7cbcb278652bc803b7",
+    "url": "/static/media/student.771e7687.png"
+  },
+  {
+    "revision": "b8dff3652d06b0a0a8e6070e623950e2",
+    "url": "/static/media/teacher.b8dff365.png"
+  }
+]);
--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5ca421a2adbdae26f511688c62c0ab53.js"
+  "/precache-manifest.ff8f3988f061d2473231e57cdd340e08.js"
 );
 
 self.addEventListener('message', (event) => {
